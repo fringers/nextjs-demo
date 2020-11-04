86d892e8ae1b39a1a8d9d1b6f6a3cf6ffc4493ae
Jakub Król <jakub.krol@mc2innovations.com>
2020-11-04 13:31:25 +0100

    head moved to _app.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,6 @@
-import Head from 'next/head'
-
 export default function Home() {
     return (
         <div>
-            <Head>
-                <title>Fringers Next.js Demo</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-
             <h1>Witamy w naszej!</h1>
         </div>
     )
