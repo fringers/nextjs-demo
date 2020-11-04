84da3367fc0019ca88d012987301fd7b2b4a938c
Jakub Król <jakub.krol@mc2innovations.com>
2020-11-04 14:47:06 +0100

    gallery homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,33 @@
 import Link from 'next/link'
+import {getAllImages, Image} from "../lib/gallery";
 
-export default function Home() {
+export default function Home({ images }) {
     return (
         <div>
-            <h1>Witamy w naszej!</h1>
+            <h1>Witamy na nasze stronie!</h1>
 
-            <Link href="/about"><a>O projekcie</a></Link>
+            <p>
+                <Link href="/about"><a>O projekcie</a></Link>
+            </p>
+
+            <div>
+                {Object.entries(images).map((image: [id: string, value: Image]) => {
+                    return (
+                        <div key={image[0]}>
+                            <img src={image[1].src} alt={image[1].description} className="thumbnail"/>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const images = getAllImages()
+    return {
+        props: {
+            images
+        }
+    }
 }
